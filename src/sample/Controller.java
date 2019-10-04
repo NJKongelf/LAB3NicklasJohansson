7f778d1c8a53733b30021c1b67db5d1b3b17dfd2
Nicklas Johansson <lurvas4ever@hotmail.com>
2019-10-04 09:58:44 +0200

    Lagt till i Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+
 public class Controller {
+    @FXML
+    Button CircleButton;
+    @FXML
+    Button RectangleButton;
+    @FXML
+    Button LineButton;
+    @FXML
+    Button PointButton;
+    @FXML
+    ChoiceBox Choicebox;
+    @FXML
+    Canvas Canvas;
+    @FXML
+    ColorPicker ColorPicker;
 }
