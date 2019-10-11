cb8f64092bfb5d63f883428be73d99bf9770782f
Nicklas Johansson <lurvas4ever@hotmail.com>
2019-10-11 10:24:51 +0200

    Fixat bättre utritning av rektangel

--- src/Controller.java
+++ src/Controller.java
@@ -1,18 +1,11 @@
-package sample;
-
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-
-import java.util.Arrays;
 
 public class Controller {
     @FXML
@@ -23,12 +16,12 @@ public class Controller {
     Button LineButton;
     @FXML
     Button PointButton;
-    @FXML
-    ChoiceBox Choicebox;
+
     @FXML
     Canvas canvas;
     @FXML
     ColorPicker ColorPicker;
+
     private  GraphicsContext gc;
     private  Model model = new Model ();
     private Line line ;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 800, 480));
         primaryStage.show();
     }
 
