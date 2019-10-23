f1ed93f70355f75e206f2bb2ddfd52a3f599ec1e
Nicklas Johansson <lurvas4ever@hotmail.com>
2019-10-23 09:57:30 +0200

    Labb3 Godkönd redo

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch (args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader (getClass ().getResource ("sample.fxml"));
+        Model      model  = new Model ();
+        loader.setControllerFactory (param -> new Controller (model));
+        Parent root = loader.load ();
+
+        Controller controller = loader.getController ();
+        controller.setStage (primaryStage);
+
+        primaryStage.setTitle ("Shapes and Colors");
+        primaryStage.setScene (new Scene (root, 990, 470));
+        controller.init (primaryStage.getScene ());
+        primaryStage.show ();
+
+    }
+}
